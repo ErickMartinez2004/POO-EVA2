f22ce69a42119f443b7826df3afc1e5e460fe577
ErickMartinez2004 <124701911+ErickMartinez2004@users.noreply.github.com>
2023-03-09 19:54:05 -0600

    Add files via upload

---
+++ EVA2_6_COLISIONES_CONSTRUCTOR/src/eva2_6_colisiones_constructor/EVA2_6_COLISIONES_CONSTRUCTOR.java
@@ -0,0 +1,19 @@
+
+package eva2_6_colisiones_constructor;
+
+public class EVA2_6_COLISIONES_CONSTRUCTOR {
+
+    public static void main(String[] args) {
+        System.out.println("ANIMAL");
+       Animal animal = new Animal();
+       animal.comer();
+       animal.respirar();
+       
+        System.out.println("MAMFIFERO");
+       Mamifero mamifero = new Mamifero();
+       
+       
+       
+    }
+    
+}
