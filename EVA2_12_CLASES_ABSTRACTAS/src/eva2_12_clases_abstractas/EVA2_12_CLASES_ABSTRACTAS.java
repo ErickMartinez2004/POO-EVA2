0720a6eed7284bc4974783ffaf21ba3ef57f016a
ErickMartinez2004 <124701911+ErickMartinez2004@users.noreply.github.com>
2023-03-20 19:41:22 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,21 @@
+
+package eva2_12_clases_abstractas;
+
+
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        //NO SE PUEDEN CREAR OBJETOS (INSTANCIAS) DE CLASES ABSTRACTAS
+        PruebaAbstracta pAbs = new PruebaAbstracta();
+    }
+    
+}
+class Prueba{
+    
+}
+
+abstract class PruebaAbstracta{
+    
+}
